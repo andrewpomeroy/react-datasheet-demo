7ed5000fc9fc0d09dff41772112593af06f38396
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2020-10-09 18:55:56 -0700

    add basic spreadsheet demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDataSheet from 'react-datasheet';
 import './App.css';
+import 'react-datasheet/lib/react-datasheet.css';
 
 function App() {
+
+  const columnDefs = [
+    {
+      name: "Name",
+      id: "name",
+      inputType: "string"
+    },
+    {
+      name: "Number",
+      id: "number",
+      inputType: "number"
+    }
+  ];
+  const initialValues = [
+    [{value: "Andrew"}, {value: 120}],
+    [{value: "Alice"}, {value: 4000}],
+  ]
+  const gridInitialState = [
+    [...columnDefs.map(x => ({
+      readOnly: true,
+      className: 'header-cell',
+      value: x.name,
+    }))],
+    ...initialValues
+  ]
+
+  const [gridState, setGridState] = useState(gridInitialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="GridContainer">
+        <ReactDataSheet
+          data={gridState}
+          valueRenderer={cell => cell.value}
+          onCellsChanged={changes => {
+            const grid = gridState.map(row => [...row]);
+            changes.forEach(({ cell, row, col, value }) => {
+              grid[row][col] = { ...grid[row][col], value };
+            });
+            setGridState(grid);
+          }}
+        />
+      </div>
     </div>
   );
 }
